6a92df04d031149119845c391163008fb4e96ff1
DanielMonroy <dchuramo@unsa.edu.pe>
2023-05-19 01:12:18 -0500

    Corrigiendo ejercicio3

--- EjerciciosPropuestos/ejercicio3/index.js
+++ EjerciciosPropuestos/ejercicio3/index.js
@@ -1,17 +1,17 @@
 function getNextArequipaDay() {
-    const currentDate = new Date();
-    if(new Date(currentDate.getFullYear(), 7, currentDate.getDate()) - currentDate < 0) {
-        return new Date(currentDate.getFullYear() + 1, 7, 10);
-    }
-    return new Date(currentDate.getFullYear(), 7, 15);
+  const currentDate = new Date();
+  if(new Date(currentDate.getFullYear(), 8, currentDate.getDate()) - currentDate < 0) {
+    return new Date(currentDate.getFullYear() + 1, 8, 10);
   }
-  
-    function getDaysAwayFromArequipaDay() {
-        const currentDate = new Date();
-        const ms = getNextArequipaDay() - currentDate;
-        const days = Math.floor(ms / (1000 * 60 * 60 * 24));
-        return days;
-  }
-  
+  return new Date(currentDate.getFullYear(), 8, 15);
+}
+
+function getDaysAwayFromArequipaDay() {
+  const currentDate = new Date();
+  const ms = getNextArequipaDay() - currentDate;
+  const days = Math.floor(ms / (1000 * 60 * 60 * 24));
+  return days;
+}
+
 const daysAway = getDaysAwayFromArequipaDay();
 console.log(daysAway);

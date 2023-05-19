9bbbab36f2f517a4306c300b05f26a907533430e
DanielMonroy <dchuramo@unsa.edu.pe>
2023-05-18 23:29:16 -0500

    corrigiendo ejercicios

--- ejerciciosResueltos/ejercicio1/index.js
+++ ejerciciosResueltos/ejercicio1/index.js
@@ -6,11 +6,11 @@
 //   return a;
 // }
 
-/*function arrayGenerator(n, min, max) {
+function arrayGenerator(n, min, max) {
     return new Array(n).fill(0).map(function () {
         return Math.floor(Math.random() * (max - min) + min);
     });
   }
   
   const randomArray = arrayGenerator(5, 1, 10);
-  console.log(randomArray);*/
+  console.log(randomArray);

ca98b065b58a7f0248ad73275419572368733cba
DanielMonroy <dchuramo@unsa.edu.pe>
2023-05-18 22:50:43 -0500

    Ejercicio2

---
+++ EjerciciosPropuestos/ejercicio2/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const invertedForm = document.querySelector("#invertedForm");
+    const word = document.querySelector("#word");
+    const inverted = document.querySelector("#inverted");
+  
+    function invertWord(word) {
+        let invertedWord = "";
+    for(let idx in word) {
+        invertedWord = word[idx] + invertedWord;
+    }
+        return invertedWord;
+    }
+  
+    invertedForm.onsubmit = function() {
+        inverted.textContent = invertWord(word.value);
+        return false;
+    }
+});
